0915767dbe4e06c8377f5d1bb277985d4b52c5f9
dongapple <hg971027@gmail.com>
2023-07-21 17:52:24 +0900

    1차,js 파일 완성

--- mission02/client/js/main.js
+++ mission02/client/js/main.js
@@ -1,21 +1,42 @@
+const navigation = getNode(".nav");
+const visualImage = getNode(".visual img");
+const nickName = getNode(".nickName");
 
-/* 
+function handleSlider(e) {
+  e.preventDefault();
 
-1. 클릭 이벤트 활성화
-2. nav 클릭시 배경 색상 변경
-3. 이미지 변경
-4. 텍스트 변경
-5. 함수 분리
+  const target = e.target.closest("li");
 
-*/
+  if (!target) return;
+  const list = [...getNode("ul").children];
+  const index = attr(target, "data-index");
 
+  list.forEach((li) => removeClass(li, "is-active"));
 
+  addClass(target, "is-active");
+  setImage(index);
+  setBgColor(index);
+  setNameText(index);
+}
 
+navigation.addEventListener("click", handleSlider);
 
+function setBgColor(index) {
+  css(
+    "body",
+    "background",
+    `linear-gradient(to bottom,${data[index - 1].color})`
+  );
+}
 
+function setImage(index) {
+  attr(
+    visualImage,
+    "src",
+    `./assets/${data[index - 1].name.toLowerCase()}.jpeg`
+  );
+}
 
-
-
-
-
-
+function setNameText(index) {
+  nickName.textContent = data[index - 1].name;
+}
