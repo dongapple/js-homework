fe579af379d82a29a42dfac81eafc8945e114b7b
dongapple <hg971027@gmail.com>
2023-07-23 19:15:46 +0900

    alt 추가

--- mission02/client/js/setting.js
+++ mission02/client/js/setting.js
@@ -2,16 +2,17 @@ function setBgColor(index) {
   css(
     "body",
     "background",
-    `linear-gradient(to bottom,${data[index - 1].color})`
+    `linear-gradient(to bottom,${data[index - 1].color[0]},#000)`
   );
 }
 
-function setImage(index) {
+function setImage(index, img) {
   attr(
     visualImage,
     "src",
     `./assets/${data[index - 1].name.toLowerCase()}.jpeg`
   );
+  attr(visualImage, "alt", data[index - 1]);
 }
 
 function setNameText(index) {
